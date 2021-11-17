a6acf245a4123210da84636685fcb85987433a6f
Rodrigo Costa <rodrigokosta@gmail.com>
2021-11-16 23:30:32 -0300

    adding creating user code

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../model/user');
 
 router.get('/', (req, res) => {
-    return res.send({message: `Tudo ok com o método GET da rota de usuarios`});
-})
+    Users.find({}, (err, data) => {
+        if (err) return res.send({error: 'Erro na consulta de usuários.'});
+        return res.send(data);
+    });
+    
+});
 
-router.post('/', (req, res) => {
-    return res.send({message: `Tudo ok com o método POST da rota de usuarios`});
-})
 
 router.post('/create', (req, res) => {
-    return res.send({message: `Tudo ok com o método POST para CRIAR usuarios`});
+    //const obj = req.body;
+    // opcao desustrurando um objeto es6
+    const { email, password } = req.body;
+
+    if (!email || !password) return res.send({ error: 'Dados insuficientes para serem cadastrados!'} );
+    
+    Users.findOne({email}, (err, data) => {
+        if (err) return res.send({ error : 'Usuário não encontrado!' });
+        if (data) return res.send({ error: 'Usuário já existe!' });
+        
+        
+        Users.create(req.body, (err,data) => {
+            if (err) return res.send({ error: 'Erro ao criar o usuário!' });
+            return res.send(data);
+        });
+    });
+
+    //return res.send({message: `Tudo ok com o método POST para CRIAR usuarios`});
 })
 
 
