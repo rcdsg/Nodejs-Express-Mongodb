f2e9c8e767328d3b8ff0f946f0dea153d9e434a2
Rodrigo Costa <rodrigokosta@gmail.com>
2021-11-19 00:30:21 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,19 +4,19 @@ const config = () => {
     switch (env) {
         case 'dev':
             return{
-                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/apiteste?retryWrites=true&w=majority',
                 jwt_pass: 'front123',
                 jwt_expiresIn:'3d'
             }
         case 'hml':
             return{
-                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/apiteste?retryWrites=true&w=majority',
                 jwt_pass: 'front123',
                 jwt_expiresIn:'7d'
             }
         case 'prod':
             return{
-                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+                db_string: 'mongodb+srv://rc_admin:mongdbapindjrct21@clusterapi.n7i7b.mongodb.net/apiteste?retryWrites=true&w=majority',
                 jwt_pass: 'hgg%$¨&¨&*¨jdfskfhfhdskhj',
                 jwt_expiresIn:'7d'
             }
